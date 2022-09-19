f0037c4be438aa23d33b2ab6d75e2bb7c97af235
Nafu Satsuki <chocotto@nafuchoco.page>
2022-09-19 20:27:33 +0900

    Migrate nijimiss changes to 12.120.0

--- packages/backend/src/server/api/EndpointsModule.ts
+++ packages/backend/src/server/api/EndpointsModule.ts
@@ -119,7 +119,6 @@ import * as ep___drive_files_findByHash from './endpoints/drive/files/find-by-ha
 import * as ep___drive_files_find from './endpoints/drive/files/find.js';
 import * as ep___drive_files_show from './endpoints/drive/files/show.js';
 import * as ep___drive_files_update from './endpoints/drive/files/update.js';
-import * as ep___drive_files_uploadFromUrl from './endpoints/drive/files/upload-from-url.js';
 import * as ep___drive_folders from './endpoints/drive/folders.js';
 import * as ep___drive_folders_create from './endpoints/drive/folders/create.js';
 import * as ep___drive_folders_delete from './endpoints/drive/folders/delete.js';
@@ -435,7 +434,6 @@ const $drive_files_findByHash: Provider = { provide: 'ep:drive/files/find-by-has
 const $drive_files_find: Provider = { provide: 'ep:drive/files/find', useClass: ep___drive_files_find.default };
 const $drive_files_show: Provider = { provide: 'ep:drive/files/show', useClass: ep___drive_files_show.default };
 const $drive_files_update: Provider = { provide: 'ep:drive/files/update', useClass: ep___drive_files_update.default };
-const $drive_files_uploadFromUrl: Provider = { provide: 'ep:drive/files/upload-from-url', useClass: ep___drive_files_uploadFromUrl.default };
 const $drive_folders: Provider = { provide: 'ep:drive/folders', useClass: ep___drive_folders.default };
 const $drive_folders_create: Provider = { provide: 'ep:drive/folders/create', useClass: ep___drive_folders_create.default };
 const $drive_folders_delete: Provider = { provide: 'ep:drive/folders/delete', useClass: ep___drive_folders_delete.default };
@@ -755,7 +753,6 @@ const $fetchRss: Provider = { provide: 'ep:fetch-rss', useClass: ep___fetchRss.d
 		$drive_files_find,
 		$drive_files_show,
 		$drive_files_update,
-		$drive_files_uploadFromUrl,
 		$drive_folders,
 		$drive_folders_create,
 		$drive_folders_delete,
@@ -1069,7 +1066,6 @@ const $fetchRss: Provider = { provide: 'ep:fetch-rss', useClass: ep___fetchRss.d
 		$drive_files_find,
 		$drive_files_show,
 		$drive_files_update,
-		$drive_files_uploadFromUrl,
 		$drive_folders,
 		$drive_folders_create,
 		$drive_folders_delete,
