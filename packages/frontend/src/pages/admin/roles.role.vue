be844371b8efddafcea4ec31d5feb81878c3158f
nafu-at <satsuki@nafusoft.dev>
2023-03-03 23:12:39 +0900

    Replace error image.

--- packages/frontend/src/pages/admin/roles.role.vue
+++ packages/frontend/src/pages/admin/roles.role.vue
@@ -23,7 +23,7 @@
 						<MkPagination :pagination="usersPagination">
 							<template #empty>
 								<div class="_fullinfo">
-									<img src="https://xn--931a.moe/assets/info.jpg" class="_ghost"/>
+									<img src="https://media.nijimiss.app/assets/info.png" class="_ghost"/>
 									<div>{{ i18n.ts.noUsers }}</div>
 								</div>
 							</template>
