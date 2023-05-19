c2aee9bd55312d7868ce844903646ff68be1180b
tar_bin <tar.bin.master@gmail.com>
2023-05-19 20:18:23 +0900

    リリースノートを更新してない場合に404にならないようにリンクを修正

--- packages/frontend/src/components/MkUpdated.vue
+++ packages/frontend/src/components/MkUpdated.vue
@@ -22,7 +22,7 @@ const modal = shallowRef<InstanceType<typeof MkModal>>();
 
 const whatIsNew = () => {
 	modal.value.close();
-	window.open(`https://github.com/tar-bin/tanukey/releases/tag/${version}`, '_blank');
+	window.open('https://github.com/tanukey-dev/tanukey/releases', '_blank');
 };
 
 onMounted(() => {
