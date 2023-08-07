408e4203bbd55ce18c8a0b0f2d18e14fac1753e1
tar_bin <tar.bin.master@gmail.com>
2023-08-07 20:15:57 +0900

    タイムラインのピンどめの折りたたみを記憶するように修正

--- packages/frontend/src/components/MkTimelineWithScroll.vue
+++ packages/frontend/src/components/MkTimelineWithScroll.vue
@@ -2,7 +2,7 @@
 <div v-if="queue > 0" :class="$style.new"><button class="_buttonPrimary" @click="top()">{{ i18n.ts.newNoteRecived }}</button></div>
 <XTutorial v-if="$i && defaultStore.reactiveState.timelineTutorial.value != -1" class="_panel" style="margin-bottom: var(--margin);"/>
 <MkPostForm v-if="defaultStore.reactiveState.showFixedPostForm.value" :class="$style.postForm" class="post-form _panel" fixed style="margin-bottom: var(--margin);"/>
-<MkFoldableSection v-if="channel && channel.pinnedNotes?.length > 0">
+<MkFoldableSection v-if="channel && channel.pinnedNotes?.length > 0" :persistKey="channel.id">
 	<template #header><i class="ti ti-pin ti-fw" style="margin-right: 0.5em;"></i>{{ i18n.ts.pinnedNotes }}</template>
 	<div v-if="channel.pinnedNotes.length > 0" class="_gaps">
 		<MkNote v-for="note in channel.pinnedNotes" :key="note.id" class="_panel" :note="note"/>
