adebdb91aa68bdbbc4f3d8f7c25141dd81fc974c
tar_bin <tar.bin.master@gmail.com>
2023-08-18 20:38:10 +0900

    固定チャンネルの設定上限を50個に緩和

--- packages/frontend/src/pages/settings/pinnedChannel.vue
+++ packages/frontend/src/pages/settings/pinnedChannel.vue
@@ -3,7 +3,7 @@
 	<FormSlot>
 		<div :class="$style.metadataRoot">
 			<div :class="$style.metadataMargin">
-				<MkButton :disabled="fields.length >= 16" inline style="margin-right: 8px;" @click="addField"><i class="ti ti-plus"></i> {{ i18n.ts.add }}</MkButton>
+				<MkButton :disabled="fields.length >= 50" inline style="margin-right: 8px;" @click="addField"><i class="ti ti-plus"></i> {{ i18n.ts.add }}</MkButton>
 				<MkButton v-if="!fieldEditMode" :disabled="fields.length <= 1" inline danger style="margin-right: 8px;" @click="fieldEditMode = !fieldEditMode"><i class="ti ti-trash"></i> {{ i18n.ts.delete }}</MkButton>
 				<MkButton v-else inline style="margin-right: 8px;" @click="fieldEditMode = !fieldEditMode"><i class="ti ti-arrows-sort"></i> {{ i18n.ts.rearrange }}</MkButton>
 				<MkButton inline primary @click="saveFields"><i class="ti ti-check"></i> {{ i18n.ts.save }}</MkButton>
