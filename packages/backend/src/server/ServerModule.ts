196554d9d8251ce54a183f56e29abd154281f023
Nafu Satsuki <satsuki@nafusoft.dev>
2023-12-30 14:15:41 +0900

    wip(subscription): エンドポイントを登録する

--- packages/backend/src/server/ServerModule.ts
+++ packages/backend/src/server/ServerModule.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EndpointsModule } from '@/server/api/EndpointsModule.js';
 import { CoreModule } from '@/core/CoreModule.js';
+import { StripeWebhookServerService } from '@/server/StripeWebhookServerService.js';
 import { ApiCallService } from './api/ApiCallService.js';
 import { FileServerService } from './FileServerService.js';
 import { NodeinfoServerService } from './NodeinfoServerService.js';
@@ -80,8 +81,8 @@ import { RoleTimelineChannelService } from './api/stream/channels/role-timeline.
 		ServerStatsChannelService,
 		UserListChannelService,
 		OpenApiServerService,
-	],
-	exports: [
+		StripeWebhookServerService,
+	],	exports: [
 		ServerService,
 	],
 })
--- packages/backend/src/server/api/endpoints.ts
+++ packages/backend/src/server/api/endpoints.ts
@@ -308,6 +308,9 @@ import * as ep___roles_list from './endpoints/roles/list.js';
 import * as ep___roles_show from './endpoints/roles/show.js';
 import * as ep___roles_users from './endpoints/roles/users.js';
 import * as ep___roles_notes from './endpoints/roles/notes.js';
+import * as ep___subscription_checkout from './endpoints/subscription/checkout.js';
+import * as ep___subscription_manage from './endpoints/subscription/manage.js';
+import * as ep___subscription_getAvailablePlans from './endpoints/subscription/get-available-plans.js';
 import * as ep___requestResetPassword from './endpoints/request-reset-password.js';
 import * as ep___resetDb from './endpoints/reset-db.js';
 import * as ep___resetPassword from './endpoints/reset-password.js';
@@ -668,6 +671,9 @@ const eps = [
 	['roles/show', ep___roles_show],
 	['roles/users', ep___roles_users],
 	['roles/notes', ep___roles_notes],
+	['subscription/checkout', ep___subscription_checkout],
+	['subscription/manage', ep___subscription_manage],
+	['subscription/get-available-plans', ep___subscription_getAvailablePlans],
 	['request-reset-password', ep___requestResetPassword],
 	['reset-db', ep___resetDb],
 	['reset-password', ep___resetPassword],
