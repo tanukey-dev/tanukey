fea3ba5431225a646d3f3fa3e34464a817f7287b
tar_bin <tar.bin.master@gmail.com>
2024-03-13 17:44:22 +0000

    nodeinfoのsoftware.nameをtanukeyに変更

--- packages/backend/src/server/NodeinfoServerService.ts
+++ packages/backend/src/server/NodeinfoServerService.ts
@@ -77,7 +77,7 @@ export class NodeinfoServerService {
 
 			return {
 				software: {
-					name: 'misskey',
+					name: 'tanukey',
 					version: this.config.version,
 					repository: meta.repositoryUrl,
 				},
