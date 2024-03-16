c36ad7f112b488e7b0d86c8b68b83a37b13ca083
tar_bin <tar.bin.master@gmail.com>
2024-03-16 20:23:10 +0000

    言語関係のエラーの修正

--- locales/index.js
+++ locales/index.js
@@ -14,32 +14,9 @@ const merge = (...args) => args.reduce((a, c) => ({
 }), {});
 
 const languages = [
-	'ar-SA',
-	'cs-CZ',
-	'da-DK',
-	'de-DE',
 	'en-US',
-	'es-ES',
-	'fr-FR',
-	'id-ID',
-	'it-IT',
 	'ja-JP',
 	'ja-KS',
-	'kab-KAB',
-	'kn-IN',
-	'ko-KR',
-	'nl-NL',
-	'no-NO',
-	'pl-PL',
-	'pt-PT',
-	'ru-RU',
-	'sk-SK',
-	'th-TH',
-	'ug-CN',
-	'uk-UA',
-	'vi-VN',
-	'zh-CN',
-	'zh-TW',
 ];
 
 const primaries = {
