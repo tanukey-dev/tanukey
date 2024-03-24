cdce53c73846665ca98aa6e9d822101804c729b0
tar_bin <tar.bin.master@gmail.com>
2024-03-24 17:31:45 +0000

    他チャンネルに投稿したタグも拾うように修正

--- packages/backend/src/core/NoteCreateService.ts
+++ packages/backend/src/core/NoteCreateService.ts
@@ -362,22 +362,22 @@ export class NoteCreateService implements OnApplicationShutdown {
 				'MAXLEN', '~', '1000',
 				'*',
 				'note', note.id);
-		} else {
-			if (note.userHost == null && tags.length > 0) {
-				// タグが該当するチャンネルのタイムラインキャッシュに設定
-				const channelQuery = this.channelsRepository.createQueryBuilder('channel');
-				for (const tag of tags) {
-					if (!safeForSql(normalizeForSearch(tag))) continue;
-					channelQuery.orWhere(`'{"${normalizeForSearch(tag)}"}' <@ channel.tags`);
-				}
-				const tagsChannel = await channelQuery.getMany();
-				for (const tagCh of tagsChannel) {
-					this.redisClient.xadd(
-						`channelTimeline:${tagCh.id}`,
-						'MAXLEN', '~', '1000',
-						'*',
-						'note', note.id);
-				}
+		}
+
+		// タグが該当するチャンネルのタイムラインキャッシュに設定
+		if (note.userHost == null && tags.length > 0) {
+			const channelQuery = this.channelsRepository.createQueryBuilder('channel');
+			for (const tag of tags) {
+				if (!safeForSql(normalizeForSearch(tag))) continue;
+				channelQuery.orWhere(`'{"${normalizeForSearch(tag)}"}' <@ channel.tags`);
+			}
+			const tagsChannel = await channelQuery.getMany();
+			for (const tagCh of tagsChannel) {
+				this.redisClient.xadd(
+					`channelTimeline:${tagCh.id}`,
+					'MAXLEN', '~', '1000',
+					'*',
+					'note', note.id);
 			}
 		}
 
