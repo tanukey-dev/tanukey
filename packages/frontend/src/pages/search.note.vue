21c7c4fae1cc5d3ec9df4445df4da1d8f48ecc01
tar_bin <tar.bin.master@gmail.com>
2024-09-22 18:15:17 +0000

    検索の開始日・終了日の時刻が正しく補正されてなかった不具合の修正

--- packages/frontend/src/pages/search.note.vue
+++ packages/frontend/src/pages/search.note.vue
@@ -1,61 +1,64 @@
 <template>
-<div class="_gaps">
 	<div class="_gaps">
-		<MkInput v-model="searchQuery" :large="true" :autofocus="true" type="search">
-			<template #prefix><i class="ti ti-search"></i></template>
-		</MkInput>
-		<MkFolder :defaultOpen="optionOpen">
-			<template #label>{{ i18n.ts.options }}</template>
+		<div class="_gaps">
+			<MkInput v-model="searchQuery" :large="true" :autofocus="true" type="search">
+				<template #prefix><i class="ti ti-search"></i></template>
+			</MkInput>
+			<MkFolder :defaultOpen="optionOpen">
+				<template #label>{{ i18n.ts.options }}</template>
 
-			<MkFolder :class="$style.formContent" :defaultOpen="userOptionOpen">
-				<template #label>{{ i18n.ts.specifyUser }}</template>
-				<template v-if="user" #suffix>@{{ user.username }}</template>
+				<MkFolder :class="$style.formContent" :defaultOpen="userOptionOpen">
+					<template #label>{{ i18n.ts.specifyUser }}</template>
+					<template v-if="user" #suffix>@{{ user.username }}</template>
 
-				<div style="text-align: center;" class="_gaps">
-					<div v-if="user">@{{ user.username }}</div>
-					<div>
-						<MkButton v-if="user == null" primary rounded inline @click="selectUser">{{ i18n.ts.selectUser }}</MkButton>
-						<MkButton v-else danger rounded inline @click="user = null">{{ i18n.ts.remove }}</MkButton>
+					<div style="text-align: center;" class="_gaps">
+						<div v-if="user">@{{ user.username }}</div>
+						<div>
+							<MkButton v-if="user == null" primary rounded inline @click="selectUser">{{
+								i18n.ts.selectUser }}
+							</MkButton>
+							<MkButton v-else danger rounded inline @click="user = null">{{ i18n.ts.remove }}</MkButton>
+						</div>
 					</div>
-				</div>
+				</MkFolder>
+
+				<FormSplit :class="$style.formContent">
+					<MkInput v-model="createAtBegin" type="datetime-local">
+						<template #label>{{ i18n.ts.startDate }}</template>
+					</MkInput>
+					<MkInput v-model="createAtEnd" type="datetime-local">
+						<template #label>{{ i18n.ts.endDate }}</template>
+					</MkInput>
+				</FormSplit>
+
+				<MkSwitch v-model="reverseOrder" :class="$style.formContent">
+					{{ i18n.ts.olderOrder }}
+				</MkSwitch>
+
+				<MkSwitch v-model="hasFile" :class="$style.formContent">
+					{{ i18n.ts.withFileAntenna }}
+				</MkSwitch>
 			</MkFolder>
-
-			<FormSplit :class="$style.formContent">
-				<MkInput v-model="createAtBegin" type="datetime-local">
-					<template #label>{{ i18n.ts.startDate }}</template>
-				</MkInput>
-				<MkInput v-model="createAtEnd" type="datetime-local">
-					<template #label>{{ i18n.ts.endDate }}</template>
-				</MkInput>
-			</FormSplit>
-
-			<MkSwitch v-model="reverseOrder" :class="$style.formContent">
-				{{ i18n.ts.olderOrder }}
-			</MkSwitch>
-
-			<MkSwitch v-model="hasFile" :class="$style.formContent">
-				{{ i18n.ts.withFileAntenna }}
-			</MkSwitch>
-		</MkFolder>
-		<MkRadios v-model="searchOrigin">
-			<option value="local">{{ i18n.ts.local }}</option>
-			<option value="remote">{{ i18n.ts.remote }}</option>
-			<option value="combined">{{ i18n.ts.all }}</option>
-		</MkRadios>
-		<div>
-			<MkButton large primary gradate rounded style="margin: 0 auto;" @click="search">{{ i18n.ts.search }}</MkButton>
+			<MkRadios v-model="searchOrigin">
+				<option value="local">{{ i18n.ts.local }}</option>
+				<option value="remote">{{ i18n.ts.remote }}</option>
+				<option value="combined">{{ i18n.ts.all }}</option>
+			</MkRadios>
+			<div>
+				<MkButton large primary gradate rounded style="margin: 0 auto;" @click="search">{{ i18n.ts.search }}
+				</MkButton>
+			</div>
 		</div>
-	</div>
 
-	<MkFoldableSection v-if="notePagination">
-		<template #header>{{ i18n.ts.searchResult }}</template>
-		<MkNotes :key="key" :pagination="notePagination"/>
-	</MkFoldableSection>
-</div>
+		<MkFoldableSection v-if="notePagination">
+			<template #header>{{ i18n.ts.searchResult }}</template>
+			<MkNotes :key="key" :pagination="notePagination" />
+		</MkFoldableSection>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref, watch } from "vue";
+import { ref, watch } from "vue";
 import MkNotes from "@/components/MkNotes.vue";
 import MkInput from "@/components/MkInput.vue";
 import MkRadios from "@/components/MkRadios.vue";
@@ -81,10 +84,6 @@ let reverseOrder = ref<any>(false);
 let hasFile = ref<any>(false);
 let optionOpen = ref(false);
 let userOptionOpen = ref(false);
-
-// ISO形式はTZがUTCになってしまうので、TZ分ずらして時間を初期化
-const localTime = new Date();
-const localTimeDiff = localTime.getTimezoneOffset() * 60 * 1000;
 
 watch(searchQuery, () => {
 	updateUrlParameter();
@@ -209,11 +208,9 @@ async function search() {
 	let end: Date | null = null;
 	if (createAtBegin.value !== null) {
 		begin = new Date(createAtBegin.value);
-		begin.setMilliseconds(begin.getMilliseconds() - localTimeDiff);
 	}
 	if (createAtEnd.value !== null) {
 		end = new Date(createAtEnd.value);
-		end.setMilliseconds(end.getMilliseconds() - localTimeDiff);
 	}
 
 	notePagination = {
