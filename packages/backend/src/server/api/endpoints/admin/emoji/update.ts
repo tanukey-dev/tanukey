1ffae6c3b8c4aa8028ef6b557ac3995e2e3fc00c
tar_bin <tar.bin.master@gmail.com>
2024-09-29 16:52:15 +0000

    Revert "リモートインポートの絵文字が更新できない不具合の修正"
    This reverts commit 4ecb7b09f9a239eee609b79a0e52bc53c354c7ab.

--- packages/backend/src/server/api/endpoints/admin/emoji/update.ts
+++ packages/backend/src/server/api/endpoints/admin/emoji/update.ts
@@ -1,70 +1,59 @@
-import type { CustomEmojiService } from "@/core/CustomEmojiService.js";
-import { DI } from "@/di-symbols.js";
-import type { DriveFilesRepository, EmojisRepository } from "@/models/index.js";
-import { Endpoint } from "@/server/api/endpoint-base.js";
-import { Inject, Injectable } from "@nestjs/common";
-import { ApiError } from "../../../error.js";
+import { Inject, Injectable } from '@nestjs/common';
+import { Endpoint } from '@/server/api/endpoint-base.js';
+import { CustomEmojiService } from '@/core/CustomEmojiService.js';
+import type { DriveFilesRepository } from '@/models/index.js';
+import { DI } from '@/di-symbols.js';
+import { ApiError } from '../../../error.js';
 
 export const meta = {
-	tags: ["admin"],
+	tags: ['admin'],
 
 	requireCredential: true,
-	requireRolePolicy: "canManageCustomEmojis",
-	kind: "write:admin:emoji",
+	requireRolePolicy: 'canManageCustomEmojis',
+	kind: 'write:admin:emoji',
 
 	errors: {
 		noSuchEmoji: {
-			message: "No such emoji.",
-			code: "NO_SUCH_EMOJI",
-			id: "684dec9d-a8c2-4364-9aa8-456c49cb1dc8",
+			message: 'No such emoji.',
+			code: 'NO_SUCH_EMOJI',
+			id: '684dec9d-a8c2-4364-9aa8-456c49cb1dc8',
 		},
 		noSuchFile: {
-			message: "No such file.",
-			code: "NO_SUCH_FILE",
-			id: "14fb9fd9-0731-4e2f-aeb9-f09e4740333d",
+			message: 'No such file.',
+			code: 'NO_SUCH_FILE',
+			id: '14fb9fd9-0731-4e2f-aeb9-f09e4740333d',
 		},
 		sameNameEmojiExists: {
-			message: "Emoji that have same name already exists.",
-			code: "SAME_NAME_EMOJI_EXISTS",
-			id: "7180fe9d-1ee3-bff9-647d-fe9896d2ffb8",
-		},
-		permissionDenied: {
-			message: "permission denied.",
-			code: "PERMISSION_DENIED",
-			id: "684dec9d-a8c3-4364-9aa8-456c49cb2dc1",
+			message: 'Emoji that have same name already exists.',
+			code: 'SAME_NAME_EMOJI_EXISTS',
+			id: '7180fe9d-1ee3-bff9-647d-fe9896d2ffb8',
 		},
 	},
 } as const;
 
 export const paramDef = {
-	type: "object",
+	type: 'object',
 	properties: {
-		id: { type: "string", format: "misskey:id" },
-		name: { type: "string", pattern: "^[a-zA-Z0-9_]+$" },
-		fileId: { type: "string", format: "misskey:id" },
+		id: { type: 'string', format: 'misskey:id' },
+		name: { type: 'string', pattern: '^[a-zA-Z0-9_]+$' },
+		fileId: { type: 'string', format: 'misskey:id' },
 		category: {
-			type: "string",
+			type: 'string',
 			nullable: true,
-			description: "Use `null` to reset the category.",
+			description: 'Use `null` to reset the category.',
 		},
-		aliases: {
-			type: "array",
-			items: {
-				type: "string",
-			},
-		},
-		license: { type: "string", nullable: true },
-		draft: { type: "boolean" },
-		isSensitive: { type: "boolean" },
-		localOnly: { type: "boolean" },
-		roleIdsThatCanBeUsedThisEmojiAsReaction: {
-			type: "array",
-			items: {
-				type: "string",
-			},
-		},
+		aliases: { type: 'array', items: {
+			type: 'string',
+		} },
+		license: { type: 'string', nullable: true },
+		draft: { type: 'boolean' },
+		isSensitive: { type: 'boolean' },
+		localOnly: { type: 'boolean' },
+		roleIdsThatCanBeUsedThisEmojiAsReaction: { type: 'array', items: {
+			type: 'string',
+		} },
 	},
-	required: ["id", "name", "aliases", "draft"],
+	required: ['id', 'name', 'aliases', 'draft'],
 } as const;
 
 // eslint-disable-next-line import/no-default-export
@@ -74,36 +63,16 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 		@Inject(DI.driveFilesRepository)
 		private driveFilesRepository: DriveFilesRepository,
 
-		@Inject(DI.emojisRepository)
-		private emojisRepository: EmojisRepository,
-
 		private customEmojiService: CustomEmojiService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
 			let driveFile = null;
 
-			// 元の絵文字申請者と一致するかのチェック、絵文字が削除されている場合はだれでもよい
-			const oldEmoji = await this.emojisRepository.findOneByOrFail({
-				id: ps.id,
-			});
-			if (oldEmoji.driveFileId) {
-				const oldDriveFile = await this.driveFilesRepository.findOneBy({
-					id: oldEmoji.driveFileId,
-				});
-				if (oldDriveFile) {
-					if (oldDriveFile.userHost === null && me.id !== oldDriveFile.userId) {
-						throw new ApiError(meta.errors.permissionDenied);
-					}
-				}
-			}
-
 			if (ps.fileId) {
-				driveFile = await this.driveFilesRepository.findOneBy({
-					id: ps.fileId,
-				});
+				driveFile = await this.driveFilesRepository.findOneBy({ id: ps.fileId });
 				if (driveFile == null) throw new ApiError(meta.errors.noSuchFile);
 			}
-
+	
 			await this.customEmojiService.update(ps.id, {
 				driveFile: driveFile,
 				name: ps.name,
@@ -113,8 +82,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				draft: ps.draft,
 				isSensitive: ps.isSensitive,
 				localOnly: ps.localOnly,
-				roleIdsThatCanBeUsedThisEmojiAsReaction:
-					ps.roleIdsThatCanBeUsedThisEmojiAsReaction,
+				roleIdsThatCanBeUsedThisEmojiAsReaction: ps.roleIdsThatCanBeUsedThisEmojiAsReaction,
 			});
 		});
 	}
