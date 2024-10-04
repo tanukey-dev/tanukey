70c737c9fe8ab72814abf9eb65a721355f6ac504
tar_bin <tar.bin.master@gmail.com>
2024-10-04 20:10:48 +0000

    収集タグの投稿が即時反映されるように

--- packages/backend/src/server/api/stream/channels/channel.ts
+++ packages/backend/src/server/api/stream/channels/channel.ts
@@ -1,8 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import type { NoteEntityService } from "@/core/entities/NoteEntityService.js";
+import { bindThis } from "@/decorators.js";
+import { DI } from "@/di-symbols.js";
 import { isUserRelated } from "@/misc/is-user-related.js";
 import type { Packed } from "@/misc/json-schema.js";
-import { NoteEntityService } from "@/core/entities/NoteEntityService.js";
-import { bindThis } from "@/decorators.js";
+import type { Channel as _Channel } from "@/models/entities/Channel.js";
+import type { ChannelsRepository } from "@/models/index.js";
+import { Inject, Injectable } from "@nestjs/common";
 import Channel from "../channel.js";
 
 class ChannelChannel extends Channel {
@@ -10,20 +13,26 @@ class ChannelChannel extends Channel {
 	public static shouldShare = false;
 	public static requireCredential = false as const;
 	private channelId: string;
+	private channel: _Channel | null;
 
 	constructor(
 		private noteEntityService: NoteEntityService,
+		private channelsRepository: ChannelsRepository,
 
 		id: string,
 		connection: Channel["connection"],
 	) {
 		super(id, connection);
-		//this.onNote = this.onNote.bind(this);
 	}
 
 	@bindThis
 	public async init(params: any) {
 		this.channelId = params.channelId as string;
+		this.channel = await this.channelsRepository.findOne({
+			where: {
+				id: this.channelId,
+			},
+		});
 
 		// Subscribe stream
 		this.subscriber.on("notesStream", this.onNote);
@@ -31,7 +40,19 @@ class ChannelChannel extends Channel {
 
 	@bindThis
 	private async onNote(note: Packed<"Note">) {
-		if (note.channelId !== this.channelId) return;
+		if (note.channelId !== this.channelId) {
+			if (!note.tags) {
+				return;
+			}
+
+			if (!this.channel) return;
+
+			// 収集タグ
+			const tags: string[] = this.channel.tags;
+			if (note.tags.filter((item) => tags.includes(item)).length === 0) {
+				return;
+			}
+		}
 
 		// リプライなら再pack
 		if (note.replyId != null) {
@@ -80,10 +101,21 @@ export class ChannelChannelService {
 	public readonly requireCredential = ChannelChannel.requireCredential;
 	public readonly kind = ChannelChannel.kind;
 
-	constructor(private noteEntityService: NoteEntityService) {}
+	constructor(
+		@Inject(DI.noteEntityService)
+		private noteEntityService: NoteEntityService,
+
+		@Inject(DI.channelsRepository)
+		private channelsRepository: ChannelsRepository,
+	) {}
 
 	@bindThis
 	public create(id: string, connection: Channel["connection"]): ChannelChannel {
-		return new ChannelChannel(this.noteEntityService, id, connection);
+		return new ChannelChannel(
+			this.noteEntityService,
+			this.channelsRepository,
+			id,
+			connection,
+		);
 	}
 }
