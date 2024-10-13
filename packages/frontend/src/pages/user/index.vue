e36aa6d40062af72628827a59796fc9ab1546bbb
tar_bin <tar.bin.master@gmail.com>
2024-10-13 17:31:33 +0000

    プロフィールページの初期表示画面が概要にならない不具合の修正

--- packages/frontend/src/pages/user/index.vue
+++ packages/frontend/src/pages/user/index.vue
@@ -1,22 +1,24 @@
 <template>
-<MkStickyContainer>
-	<template #header><MkPageHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs"/></template>
-	<div>
-		<div v-if="user">
-			<XHome v-if="tab === 'home'" :user="user"/>
-			<XTimeline v-if="tab === 'notes'" :user="user"/>
-			<XActivity v-else-if="tab === 'activity'" :user="user"/>
-			<XAchievements v-else-if="tab === 'achievements'" :user="user"/>
-			<XReactions v-else-if="tab === 'reactions'" :user="user"/>
-			<XClips v-else-if="tab === 'clips'" :user="user"/>
-			<XLists v-else-if="tab === 'lists'" :user="user"/>
-			<XPages v-else-if="tab === 'pages'" :user="user"/>
-			<XGallery v-else-if="tab === 'gallery'" :user="user"/>
+	<MkStickyContainer>
+		<template #header>
+			<MkPageHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs" />
+		</template>
+		<div>
+			<div v-if="user">
+				<XHome v-if="tab === 'home'" :user="user" />
+				<XTimeline v-if="tab === 'notes'" :user="user" />
+				<XActivity v-else-if="tab === 'activity'" :user="user" />
+				<XAchievements v-else-if="tab === 'achievements'" :user="user" />
+				<XReactions v-else-if="tab === 'reactions'" :user="user" />
+				<XClips v-else-if="tab === 'clips'" :user="user" />
+				<XLists v-else-if="tab === 'lists'" :user="user" />
+				<XPages v-else-if="tab === 'pages'" :user="user" />
+				<XGallery v-else-if="tab === 'gallery'" :user="user" />
+			</div>
+			<MkError v-else-if="error" @retry="fetchUser()" />
+			<MkLoading v-else />
 		</div>
-		<MkError v-else-if="error" @retry="fetchUser()"/>
-		<MkLoading v-else/>
-	</div>
-</MkStickyContainer>
+	</MkStickyContainer>
 </template>
 
 <script lang="ts" setup>
@@ -42,14 +44,12 @@ const XGallery = defineAsyncComponent(() => import("./gallery.vue"));
 const props = withDefaults(
 	defineProps<{
 		acct: string;
-		page?: string;
 	}>(),
 	{
-		page: "home",
 	},
 );
 
-let tab = $ref(props.page);
+let tab = $ref("home");
 let user = $ref<null | misskey.entities.UserDetailed>(null);
 let error = $ref(null);
 
@@ -74,60 +74,60 @@ const headerActions = $computed(() => []);
 const headerTabs = $computed(() =>
 	user
 		? [
-				{
-					key: "home",
-					title: i18n.ts.overview,
-					icon: "ti ti-home",
-				},
-				{
-					key: "notes",
-					title: i18n.ts.notes,
-					icon: "ti ti-pencil",
-				},
-				{
-					key: "activity",
-					title: i18n.ts.activity,
-					icon: "ti ti-chart-line",
-				},
-				...(user.host == null
-					? [
-							{
-								key: "achievements",
-								title: i18n.ts.achievements,
-								icon: "ti ti-medal",
-							},
-						]
-					: []),
-				...(($i && $i.id === user.id) || user.publicReactions
-					? [
-							{
-								key: "reactions",
-								title: i18n.ts.reaction,
-								icon: "ti ti-mood-happy",
-							},
-						]
-					: []),
-				{
-					key: "clips",
-					title: i18n.ts.clips,
-					icon: "ti ti-paperclip",
-				},
-				{
-					key: "lists",
-					title: i18n.ts.lists,
-					icon: "ti ti-list",
-				},
-				{
-					key: "pages",
-					title: i18n.ts.pages,
-					icon: "ti ti-news",
-				},
-				{
-					key: "gallery",
-					title: i18n.ts.gallery,
-					icon: "ti ti-icons",
-				},
-			]
+			{
+				key: "home",
+				title: i18n.ts.overview,
+				icon: "ti ti-home",
+			},
+			{
+				key: "notes",
+				title: i18n.ts.notes,
+				icon: "ti ti-pencil",
+			},
+			{
+				key: "activity",
+				title: i18n.ts.activity,
+				icon: "ti ti-chart-line",
+			},
+			...(user.host == null
+				? [
+					{
+						key: "achievements",
+						title: i18n.ts.achievements,
+						icon: "ti ti-medal",
+					},
+				]
+				: []),
+			...(($i && $i.id === user.id) || user.publicReactions
+				? [
+					{
+						key: "reactions",
+						title: i18n.ts.reaction,
+						icon: "ti ti-mood-happy",
+					},
+				]
+				: []),
+			{
+				key: "clips",
+				title: i18n.ts.clips,
+				icon: "ti ti-paperclip",
+			},
+			{
+				key: "lists",
+				title: i18n.ts.lists,
+				icon: "ti ti-list",
+			},
+			{
+				key: "pages",
+				title: i18n.ts.pages,
+				icon: "ti ti-news",
+			},
+			{
+				key: "gallery",
+				title: i18n.ts.gallery,
+				icon: "ti ti-icons",
+			},
+		]
 		: [],
 );
 
@@ -135,18 +135,18 @@ definePageMetadata(
 	computed(() =>
 		user
 			? {
-					icon: "ti ti-user",
-					title: user.name
-						? `${user.name} (@${user.username})`
-						: `@${user.username}`,
-					subtitle: `@${getAcct(user)}`,
-					userName: user,
-					avatar: user,
-					path: `/@${user.username}`,
-					share: {
-						title: user.name,
-					},
-				}
+				icon: "ti ti-user",
+				title: user.name
+					? `${user.name} (@${user.username})`
+					: `@${user.username}`,
+				subtitle: `@${getAcct(user)}`,
+				userName: user,
+				avatar: user,
+				path: `/@${user.username}`,
+				share: {
+					title: user.name,
+				},
+			}
 			: null,
 	),
 );
